169ddf2251423f812adf3fc9add02a3cbefaedab
RaphaelfsOliveira <raphaelbrf@gmail.com>
2016-12-09 11:39:17 -0200

    Test git

--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Naaaaaoo PEEEIIDAA!!!")
+    return HttpResponse("Naaaaaoo PEEEIIDAA!")
