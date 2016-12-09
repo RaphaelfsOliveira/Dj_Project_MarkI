7b595e3a54809dee078f3afe3fecdb10162df14b
RaphaelfsOliveira <raphaelbrf@gmail.com>
2016-12-09 17:01:11 -0200

    make other HttpResponse

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Naaaaaoo PEEEIIDAA!")
+    return HttpResponse("Index Rango application <br/><a href='/rango/about'>About</a>")
 
 def about(request):
-    return HttpResponse('Rango says here is the ABOUT PAGE')
+    return HttpResponse("Rango says here is the ABOUT PAGE <br/><a href='/rango/'>Index</a>")
