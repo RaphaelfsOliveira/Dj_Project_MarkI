e671beb8e9730d3ea854778096277291cb4c23aa
RaphaelfsOliveira <raphaelbrf@gmail.com>
2016-12-14 16:00:21 -0200

    static file config:   dj_markI/settings.py 	img static file:   dj_markI/static/images/rango.jpg 	static file config:   dj_markI/templates/rango/index.html 	static file config:   rango/views.py

--- rango/views.py
+++ rango/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponse
 
 def index(request):
     #return HttpResponse("Index Rango application <br/><a href='/rango/about'>About</a>")
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    context_dict = {
+        'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!",
+        'lizard': "The Green lizard!!!"
+        }
     return render(request, 'rango/index.html', context=context_dict)
-    
+
 def about(request):
     return HttpResponse("Rango says here is the ABOUT PAGE <br/><br/><a href='/rango/'>Index</a>")
