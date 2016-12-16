b32b46a13c4a37606773465f464b7b9623b0c616
RaphaelfsOliveira <raphaelbrf@gmail.com>
2016-12-16 16:04:34 -0200

    make other template

--- rango/views.py
+++ rango/views.py
@@ -7,8 +7,13 @@ def index(request):
     context_dict = {
         'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!",
         'lizard': "The Green lizard!!!"
-        }
+    }
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the ABOUT PAGE <br/><br/><a href='/rango/'>Index</a>")
+    #return HttpResponse("Rango says here is the ABOUT PAGE <br/><br/><a href='/rango/'>Index</a>")
+    context_dict = {
+        'about_page': 'Page 2 ABOUT PAGE',
+        'example': 'Deadpool!!'
+    }
+    return render(request, 'rango/about.html', context=context_dict)
