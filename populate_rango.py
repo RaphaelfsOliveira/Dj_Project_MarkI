a4856316e92b4dcd175a01a9a1d37b86e9d19cc6
RaphaelfsOliveira <raphaelbrf@gmail.com>
2016-12-27 17:57:10 -0200

    populate config

--- populate_rango.py
+++ populate_rango.py
@@ -11,7 +11,7 @@ def populate():
     python_pages = [
         {"title": "Official Python Tutorial",
          "url": "http://docs.python.org/2/tutorial/"},
-        {"title": "Hou to think like a Computer Scientist",
+        {"title": "How to think like a Computer Scientist",
          "url": "http://www.greenteapress.com/thinkpython/"},
         {"title": "Learn Python in 10 Minutes",
          "url": "http://www.korokithakis.net/tutorials/python/"}
